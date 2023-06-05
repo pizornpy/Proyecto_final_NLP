26ad1c2135709c6f72474e2a5338269a05d5af2a
Juan <juanjulionn@gmail.com>
2023-06-05 17:11:06 +0200

    seguimos con streamlit, queda modelo

--- reco_libros.py
+++ reco_libros.py
@@ -9,16 +9,16 @@ import io
 import re
 
 img = Image.open(r"C:\Users\Juan\Desktop\Proyecto_final_NLP\lechuza3.png")
-#data = pd.read_csv(r"C:\Users\Juan\Desktop\Proyecto_final_NLP\phil_nlp.csv")
+#df = pd.read_csv(r"C:\Users\Juan\Desktop\Proyecto_final_NLP\phil_nlp.csv")
 st.image(img, width=100)
 st.write('# place_holder')
 
 #Background intelectual
-opciones_a = ['STEM', 'Economics/Bussiness', 'History', 'Literature', 'Philosophy' ,'Other/Surprise me']
+opciones_a = ['STEM', 'Economics/Bussiness', 'History', 'Literature', 'Philosophy','Sociology','Other/Surprise me']
 selected_option1 = st.selectbox('What is your background?', opciones_a)
 
 #Ramas de la filosofía
-opciones_b = ['Science', 'Ethic', 'History of Philosophy', 'Metaphysics', 'Politics','Aesthetics']
+opciones_b = ['Science', 'Ethics', 'History of Philosophy', 'Metaphysics', 'Politics', 'Philosophy of Art']
 selected_option2 = st.selectbox('What are you interested in?', opciones_b)
 
 
@@ -27,4 +27,25 @@ opciones_c = ['Not much', 'I have some experience', 'I have read a lot']
 selected_option3 = st.selectbox("How accustomed are you to reading Philosophy?", opciones_c)
 
 
-respuestas = {"preg1":selected_option1, "preg2":selected_option2, "preg3":selected_option3 }
+respuestas = {"preg1":selected_option1, "preg2":selected_option2, "preg3":selected_option3}
+
+if respuestas[preg1] == "STEM":
+    rec =data[data["school"] == ["analytic", "phenomenology","rationalism","empiricism"]]
+elif respuestas[preg1] =='Economics/Bussiness':
+    rec = data[data["school"] == ["capitalism", "Ethics","empiricism"]]
+elif respuestas[preg1] == "Literature":
+    rec = data[data["school"]] == ["Continental"]:
+elif respuestas[preg1] == "Philosophy":
+    rec = data[data["school"] == []]
+elif respuestas[preg1] == "History":
+    rec = data[data["school"] == ["german_idealism","phenomenology","capitalism","communism"]]
+elif respuestas[preg1] == "Other/Surprise me":
+    pass
+
+
+
+
+
+
+
+
